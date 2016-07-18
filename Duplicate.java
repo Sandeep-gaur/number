f4377bb3354e6b4425629d3ed99a30acf27ae806
Sandeep-gaur <rsandeep76@gmail.com>
2016-07-18 15:02:07 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duplicate;
+
+import java.util.Scanner;
+
+
+public class Duplicate {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Enter a string:");
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String b="";
+        char c;
+        int l=a.length();
+        for(int i=0;i<l-1;i++){
+        c=a.charAt(i);
+        if(c!=' '){
+            b=b+c;
+            a=a.replace(c,' ');
+    }}
+            System.out.println("the string is"+b);
+    
+
+    }}
